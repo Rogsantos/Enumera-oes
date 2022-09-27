c80666a2bbb2ea9a976c3b59dfd21772ef6186a8
Rogsantos <rogergoncalves_santos@hotmail.com>
2022-09-27 11:52:52 -0300

    Teste de commit

--- Enumerações/Program.cs
+++ Enumerações/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enumerações
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello word");
+        }
+    }
+}
