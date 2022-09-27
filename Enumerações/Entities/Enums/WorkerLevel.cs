4039c4412345ba58ba27d745cc8eee3caec47f43
Rogsantos <rogergoncalves_santos@hotmail.com>
2022-09-27 17:13:30 -0300

    exercicio v2

---
+++ Enumerações/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Enumerações.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ Enumerações/Entities/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enumerações.Entities.Enums;
+
+namespace Enumerações.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; }
+    }
+}
