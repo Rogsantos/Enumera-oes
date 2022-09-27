cc4709c58821cc0bab788bb26f942b149ea06bb2
Rogsantos <rogergoncalves_santos@hotmail.com>
2022-09-27 16:49:06 -0300

    Exercicio

---
+++ Enumerações/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerações.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- Enumerações/Program.cs
+++ Enumerações/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Enumerações.Entities.Enums;
 
 namespace Enumerações
 {
@@ -7,19 +6,7 @@ namespace Enumerações
     {
         static void Main(string[] args)
         {
-            Order order = new Order()
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
-
-            Console.Write(order);
-
-            string txt = OrderStatus.PendingPayment.ToString();
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-            Console.WriteLine(txt);
-            Console.WriteLine(os);
+            
         }
     }
 }
