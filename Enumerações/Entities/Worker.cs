369834e05ca17e48bd0b9366113c70d060e09d75
Roger Gonçalves <rogergoncalves_santos@hotmail.com>
2022-09-27 21:27:50 -0300

    Construtores

--- Enumerações/Entities/Worker.cs
+++ Enumerações/Entities/Worker.cs
@@ -13,6 +13,24 @@ namespace Enumerações.Entities
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
-        public List<HourContract> Contracts { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
     }
 }
